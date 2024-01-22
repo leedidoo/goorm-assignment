d5c58a860925ded5ad32a0d2b4dc3d911d5b67f4
leedidoo <665479@naver.com>
2024-01-22 14:30:57 +0900

    스프레드시트과제

--- 실습과제 03. Github Finder/github.js
+++ 실습과제 03. Github Finder/github.js
@@ -1,27 +1,28 @@
 class Github {
-    constructor() {
-       this.api_url = 'https://api.github.com/users/';
-       this.repos_count = 5;
-       this.repos_sort = 'created: asc';
-    }
+   constructor() {
+     this.client_id = 'e932ba1d163e2d200af4';
+     this.client_secret = '0e1ae9f3a7ec4b8dbdf6ab4108dd7af75030dfbb';
+     this.repos_count = 5;
+     this.repos_sort = 'created: asc';
+   }
  
-    async getUser(user) {
-       const profileResponse = 
+   async getUser(user) {
+     const profileResponse =
        await fetch(
-        `${this.api_url}${user}`);
-
-       const repoResponse = 
-       await fetch(
-          `${this.api_url}${user}/repos
-          ?per_page=${this.repos_count}&sort=${this.repos_sort}`
+         `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
        );
  
-       const profile = await profileResponse.json();
-       const repos = await repoResponse.json();
+     const repoResponse =
+       await fetch(
+         `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+       );
  
-       return {
-        profile,
-        repos
-       }
-    }
+     const profile = await profileResponse.json();
+     const repos = await repoResponse.json();
+ 
+     return {
+       profile,
+       repos
+     }
+   }
  }
